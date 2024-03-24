b6e9c5107e0facfa2f99df71e7c7e4a31d0d78db
Felipe Valencio <fgv.valencio@gmail.com>
2024-03-24 18:11:43 -0300

    mts melhorias

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
+import 'package:abelhinha/palavras_dia.dart';
 import 'package:flutter/material.dart';
 import 'package:hexagon/hexagon.dart';
+import 'package:intl/intl.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:step_progress_indicator/step_progress_indicator.dart';
 
 void main() {
@@ -36,20 +41,54 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  late final Future futureController;
+
+  late List<String> pangrams;
+
   final Map<String, String> letras = {
-    '0-1': 'R',
-    '-10': 'S',
-    '-11': 'T',
-    '01': 'E',
-    '10': 'N',
-    '1-1': 'O',
+    '0-1': '',
+    '-10': '',
+    '-11': '',
+    '01': '',
+    '10': '',
+    '1-1': '',
   };
 
-  final List<String> listaPalavras = [    "ASTRONAUTA",    "ARTESANO",    "ATRAS",    "ARANHA",    "ANSEIO",    "ATENCAO",    "ARTESAO",    "RATO",    "SONATA",    "SARJETA",    "ASTEROIDE",    "TESOURO",    "ATENAS",    "ARMACAO",    "ANTENA",    "SARTANA",    "NASCENTE",    "ANTES",    "SORTE",    "ATENUAR"];
+  late List<String> answers;
 
-  final String letraCentral = "A";
+  late String letraCentral;
 
   late List<String> palavrasEncontradas = [];
+
+  late int letrasEncontradas = 0;
+
+  Future<int> prepararJogo() async {
+
+    String? letrasDia = await PalavrasDia().getLettersForDay();
+
+    letraCentral = letrasDia![0];
+
+    int i = 1;
+
+    letras.forEach((key, value) {
+      letras.update(key, (existingValue) {
+        return letrasDia[i];
+      });
+      i++;
+    });
+
+    answers = await readFile("answers-${getCurrentDate()}.txt");
+    pangrams = await readFile("pangrams-${getCurrentDate()}.txt");
+
+    return 1;
+
+  }
+
+  @override
+  void initState() {
+    futureController = prepararJogo();
+    super.initState();
+  }
 
   void rotacionar() {
     setState(() {
@@ -66,18 +105,27 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void validar() {
+    if(controller.text.length < 4) {
+      print("Palavra muito curta!");
+      return;
+    }
+
     if(palavrasEncontradas.contains(controller.text)) {
       print("PAlAVRA já encontrada!");
+      return;
     }
-    else if(listaPalavras.contains(controller.text)) {
+
+    if(answers.contains(controller.text)) {
       print("PAlAVRA ENCONTRADA");
       setState(() {
         palavrasEncontradas.add(controller.text);
         controller.text = "";
       });
-    } else {
-      print("PAlAVRA Não está no dicionário");
+      return;
     }
+
+    print("PAlAVRA Não está no dicionário");
+
   }
 
   @override
@@ -89,35 +137,49 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          score(),
-          fieldText(controller),
-          Center(
-            child: SizedBox(
-              width: width * 0.8,
-              child: buildColmeia(context, controller)
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              IconButton(
-                icon: const Icon(Icons.delete, size: 50,),
-                onPressed: () => controller.text = "",
-              ),
-              IconButton(
-                icon: const Icon(Icons.refresh, size: 50,),
-                onPressed: rotacionar,
-              ),
-              IconButton(
-                icon: const Icon(Icons.send, size: 50,),
-                onPressed: validar,
-              ),
-            ]
-          )
-        ],
+      body: FutureBuilder(
+        future: futureController,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return const CircularProgressIndicator();
+          } else if (snapshot.hasError) {
+            print(snapshot.stackTrace);
+            return Text('Error: ${snapshot.error}');
+          } else if (snapshot.data != null) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                score(),
+                fieldText(controller),
+                Center(
+                  child: SizedBox(
+                      width: width * 0.8,
+                      child: buildColmeia(context, controller)
+                  ),
+                ),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.delete, size: 50,),
+                        onPressed: () => controller.text = "",
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.refresh, size: 50,),
+                        onPressed: rotacionar,
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.send, size: 50,),
+                        onPressed: validar,
+                      ),
+                    ]
+                )
+              ],
+            );
+          }
+
+          return const Center(child: CircularProgressIndicator());
+        },
       ),
     );
   }
@@ -193,16 +255,16 @@ class _MyHomePageState extends State<MyHomePage> {
                   "0"
               ),
               Text(
-                  "${(listaPalavras.length * 0.25).floor()}"
+                  "${(answers.length * 0.25).floor()}"
               ),
               Text(
-                  "${(listaPalavras.length * 0.5).floor()}"
+                  "${(answers.length * 0.5).floor()}"
               ),
               Text(
-                  "${(listaPalavras.length * 0.75).floor()}"
+                  "${(answers.length * 0.75).floor()}"
               ),
               Text(
-                  "${listaPalavras.length}"
+                  "${answers.length}"
               ),
             ],
           ),
@@ -211,11 +273,11 @@ class _MyHomePageState extends State<MyHomePage> {
         SizedBox(
           width: MediaQuery.of(context).size.width * 0.8,
           child: StepProgressIndicator(
-            totalSteps: (listaPalavras.length / 4).floor(),
-            currentStep: (palavrasEncontradas.length / 4).floor(),
+            totalSteps: (answers.length / 100000).floor(),
+            currentStep: palavrasEncontradas.length,
+            size: 20,
             selectedColor: Colors.yellow,
             unselectedColor: Colors.grey,
-
           ),
         ),
         const SizedBox(height: 10,),
@@ -230,6 +292,18 @@ class _MyHomePageState extends State<MyHomePage> {
         )
       ],
     );
+  }
+
+  readFile(String fileName) async {
+    final Directory directory = await getApplicationDocumentsDirectory();
+    final File file = File('${directory.path}/$fileName');
+    return await file.readAsLines();
+  }
+
+  String getCurrentDate() {
+    DateTime now = DateTime.now();
+    String formattedDate = DateFormat('yyyy-MM-dd').format(now);
+    return formattedDate;
   }
 
 }
---
+++ lib/palavras_dia.dart
@@ -0,0 +1,191 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:math';
+import 'dart:async' show Future;
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:path_provider/path_provider.dart';
+import 'package:intl/intl.dart';
+
+
+class PalavrasDia {
+
+  Future<String?> getLettersForDay() async {
+
+    if(!(await checkFileExists("answers-all.txt"))) {
+      await createAllAnswersFile();
+    }
+
+    if(!(await checkFileExists("pangrams-all.txt"))) {
+      await createAllPangramsFile();
+    }
+
+    if(!(await checkFileExists("letras-all.txt"))) {
+      await createAllLettersFile();
+    }
+
+    if(!(await checkFileExists("letras-${getCurrentDate()}.txt"))) {
+      await createDayLetters();
+    }
+
+    if(!(await checkFileExists("answers-${getCurrentDate()}.txt"))) {
+      await createDayAnswers();
+    }
+
+    if(!(await checkFileExists("pangrams-${getCurrentDate()}.txt"))) {
+      await createDayPangrams();
+    }
+
+    return await readFileString("letras-${getCurrentDate()}.txt");
+  }
+
+  Future<void> createAllAnswersFile() async {
+    LineSplitter ls = const LineSplitter();
+    List<String> palavras = ls.convert(await rootBundle.loadString('assets/palavras-all.txt'));
+
+    List<String> validWords = palavras.where((word) {
+      if (word.length < 4) return false;
+      Set<String> uniqueLetters = Set<String>.from(word.split(''));
+      if (uniqueLetters.length > 7) return false;
+      return true;
+    }).toList();
+
+    await writeToFile("answers-all.txt", validWords);
+  }
+
+  Future<void> createAllPangramsFile() async {
+    if(!(await checkFileExists("answers-all.txt"))) throw "Answer all file not found";
+
+    List<String> validWords = await readFile("answers-all.txt");
+
+    List<String> pangrams = validWords.where((word) => Set<String>.from(word.split('')).length == 7).toList();
+
+    await writeToFile("pangrams-all.txt", pangrams);
+  }
+
+  Future<void> createAllLettersFile() async {
+    if(!(await checkFileExists("answers-all.txt"))) throw "Answer all file not found";
+    if(!(await checkFileExists("pangrams-all.txt"))) throw "Pangrams all file not found";
+
+    List<String> pangrams = await readFile("pangrams-all.txt");
+
+    Set<String> uniqueLetterCombinations = pangrams.fold<Set<String>>(<String>{}, (acc, pangram) {
+      Set<String> uniqueLetters = Set<String>.from(pangram.split('').toList()..sort());
+      acc.add(uniqueLetters.join(''));
+      return acc;
+    });
+
+    await writeToFile("letras-all.txt", uniqueLetterCombinations.toList());
+  }
+
+  Future<void> createDayAnswers() async {
+    if(!(await checkFileExists("answers-all.txt"))) throw "Answer all file not found";
+    if(!(await checkFileExists("letras-${getCurrentDate()}.txt"))) throw "Letras dia file not found";
+
+    List<String> words = await readFile("answers-all.txt");
+
+    String letras = await readFileString("letras-${getCurrentDate()}.txt");
+
+    List<String> letters = letras.split('');
+    String specificLetter = letras[0];
+
+    //TODO: TESTAR ESSA LOGICA
+
+    List<String> filteredWords = words.where((word) {
+      // Check if the word contains only letters from the list
+      if (!word.split('').every((char) => letters.contains(char))) {
+        return false;
+      }
+
+      // Check if the word contains the specific letter
+      if (!word.contains(specificLetter)) {
+        return false;
+      }
+
+      return true;
+    }).toList();
+
+    await writeToFile("answers-${getCurrentDate()}.txt", filteredWords);
+
+  }
+
+  Future<void> createDayPangrams() async {
+    if(!(await checkFileExists("pangrams-all.txt"))) throw "Pangrams all file not found";
+    if(!(await checkFileExists("letras-${getCurrentDate()}.txt"))) throw "Letras dia file not found";
+
+    List<String> pangrams = await readFile("pangrams-all.txt");
+
+    String letras = await readFileString("letras-${getCurrentDate()}.txt");
+
+    List<String> letters = letras.split('');
+    String specificLetter = letras[0];
+
+    List<String> filteredWords = pangrams.where((word) {
+      // Check if the word contains the specific letter
+      if (!word.contains(specificLetter)) return false;
+
+      // Check if the word contains at least one other letter from the list
+      for (String letter in letters) {
+        if (letter != specificLetter && word.contains(letter)) {
+          return true;
+        }
+      }
+      return false;
+    }).toList();
+
+    await writeToFile("pangrams-${getCurrentDate()}.txt", filteredWords);
+  }
+
+  Future<void> createDayLetters() async {
+    if(!(await checkFileExists("letras-all.txt"))) throw "Letras all file not found";
+
+    List<String> letrasAll = await readFile("letras-all.txt");
+
+    Random random = Random();
+
+    String letras = letrasAll.elementAt(random.nextInt(letrasAll.length));
+
+    await writeToFileString("letras-${getCurrentDate()}.txt", letras);
+  }
+
+  Future<void> writeToFile(String fileName, List<String> text) async {
+    final Directory directory = await getApplicationDocumentsDirectory();
+    final File file = File('${directory.path}/$fileName');
+    await file.writeAsString(text.join('\n'));
+  }
+
+  Future<void> writeToFileString(String fileName, String text) async {
+    final Directory directory = await getApplicationDocumentsDirectory();
+    final File file = File('${directory.path}/$fileName');
+    await file.writeAsString(text);
+  }
+
+  String getCurrentDate() {
+    DateTime now = DateTime.now();
+    String formattedDate = DateFormat('yyyy-MM-dd').format(now);
+    return formattedDate;
+  }
+
+  Future<bool> checkFileExists(String fileName) async {
+    final Directory directory = await getApplicationDocumentsDirectory();
+    final File file = File('${directory.path}/$fileName');
+    if (file.existsSync()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<String> readFileString(String fileName) async {
+    final Directory directory = await getApplicationDocumentsDirectory();
+    final File file = File('${directory.path}/$fileName');
+    return await file.readAsString();
+  }
+
+  Future<List<String>> readFile(String fileName) async {
+    final Directory directory = await getApplicationDocumentsDirectory();
+    final File file = File('${directory.path}/$fileName');
+    return await file.readAsLines();
+  }
+
+
+}
